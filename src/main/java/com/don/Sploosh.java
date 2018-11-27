3c9bb84b28e1d0b3c451ca5d4a9816c306ddffb3
Diarmuid O'Neill <diarmuid.oneill@gmail.com>
2018-11-27 22:14:52 +0000

    got boats and quads

--- src/main/java/com/don/Sploosh.java
+++ src/main/java/com/don/Sploosh.java
@@ -16,34 +16,78 @@ public class Sploosh {
         dice.put(10, 1);
         dice.put(20, 3);
 
-        List<Integer> rolls = genRolls(dice);
+      /*  List<Integer> rolls = genRolls(dice);
         for (Integer result : rolls) {
             System.out.println(result);
         }
-        System.out.println("Hello World!");
+        Map<Integer,Integer> outcomes = genOutcome(rolls);
+        for(Integer key: outcomes.keySet()){
+            System.out.println(outcomes.get(key) + " " + key + " of a kind");
+        }*/
+
+
+      //  AtomicInteger boats = new AtomicInteger(0);
+        //AtomicInteger quads = new AtomicInteger(0);
+        int boats=0;
+        int quads=0;
+        for(int i=0;i<1000000;i++){
+            List<Integer> rolls = genRolls(dice);
+       //     for (Integer result : rolls) {
+         //       System.out.println(result);
+         //   }
+         //   System.out.println("i is " + i);
+            Map<Integer,Integer> outcomes = genOutcome(rolls);
+
+            if (hasBoat(outcomes)) boats++;
+            if (hasQuads(outcomes)) quads++;
+        }
+
+        /*IntStream.range(0,1000000).forEach( nbr -> {
+            List<Integer> rolls = genRolls(dice);
+            Map<Integer,Integer> outcomes = genOutcome(rolls);
+            if (hasBoat(outcomes)) boats.set(boats.getAndIncrement());
+            if (hasQuads(outcomes)) quads.set(quads.getAndIncrement());
+
+        });*/
+        /*if (hasBoat(outcomes)) System.out.println("Has boat");
+        if (hasQuads(outcomes)) System.out.println("Has quads");*/
+        System.out.println("Hello World! Boats: " + boats + " quads: " + quads);
     }
-
-
-    private static List<Integer> get Result
-
     private static List<Integer> genRolls(Map<Integer, Integer> dice) {
         ArrayList rolls = new ArrayList();
-        for (Integer die : dice.keySet()) {
-            while (dice.get(die) > 0) {
+        Map<Integer, Integer> mydice = new HashMap<>(dice);
+        for (Integer die : mydice.keySet()) {
+            while (mydice.get(die) > 0) {
                 rolls.add(rand.nextInt(die)+1);
-                dice.put(die, dice.get(die)-1);
+                mydice.put(die, mydice.get(die)-1);
             }
         }
         Collections.sort(rolls);
         return rolls;
     }
-    class Outcome {
-        int boat = 0;
-        ArrayList<Integer> matches = new List<Integer>();
-        public Outcome(List rolls){
-            while (rolls.size()>0){
-
+    public static Map<Integer, Integer> genOutcome(List<Integer> rolls){
+        Map<Integer,Integer> matches = new HashMap<Integer,Integer>();
+        while (rolls.size()>1){
+            Integer roll = rolls.remove(0);
+            int count = 1;
+            while((rolls.size() > 0) && rolls.get(0).equals(roll)){
+                count++;
+                rolls.remove(0);
+            }
+            if (count > 1){
+                if(matches.containsKey(count)){
+                    matches.put(count, matches.get(count)+1);
+                } else {
+                    matches.put(count, 1);
+                }
             }
         }
+        return matches;
+    }
+    public static boolean hasBoat(Map<Integer,Integer>matches){
+        return matches.containsKey(2) && matches.containsKey(3);
+    }
+    public static boolean hasQuads(Map<Integer,Integer>matches){
+        return matches.containsKey(4);
     }
 }
